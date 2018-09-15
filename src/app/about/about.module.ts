0a35c8bfd910698b0add7e7d1ce5d313c3df1507
olda <oldrich.hobl@gmail.com>
2018-09-16 01:02:02 +0200

    Zmena ID v my-board
    Zmena ID rada1, rada2 atd v komponente my-board

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,7 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AboutPage } from './about.page';
