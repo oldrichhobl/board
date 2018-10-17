12d926df54c38f3db027c5d046575f6ae3dd15ac
olda <oldrich.hobl@gmail.com>
2018-10-17 12:44:55 +0200

    pridan Range s events
    pridan Range s events

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Events } from '@ionic/angular';
 
 @Component({
   selector: 'app-contact',
@@ -6,20 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['contact.page.scss']
 })
 export class ContactPage {
- constructor( ) {
-
+ constructor(public events: Events ) {
+    console.log("CONTRUCTOR ")
+    events.subscribe('ionChange', (user) => {
+    // user and time are the same arguments passed in `events.publish(user, time)`
+    console.log('Welcome Events', user);
+  });
 }
 
 ngOnInit() {
     console.log(" ngOnInit na ContactPage.ts");
     //svgChessBoard.addEventListener('mouseup', (e) => {
 }  
-  
+ 
+ionChange(e)
+{
+  console.log("ionChange " );
+  console.dir(e );
+  let idd = document.getElementById("c1");
+  //console.log(idd.getAttribute('cy'));
+  // posun o padesat : idd.setAttribute('cy', 50 + Number(idd.getAttribute('cy'));
+  idd.setAttribute('cy', this.sliderrange);
+
+}  
 clickPole(id) {
-     console.log("openLocalSpz");
+     console.log("openLocalSPZ " + this.sliderrange);
      console.dir(id); 
-     let idd = document.getElementById(id);
-     console.dir(idd);
+     let idd = document.getElementById("c1");
+     console.log(idd.getAttribute('cy'));
+    // posun o padesat : idd.setAttribute('cy', 50 + Number(idd.getAttribute('cy'));
+    idd.setAttribute('cy', this.sliderrange);
+     // idd.cy = 300;
      // idd.style = "fill: silver;";
    }
 }
